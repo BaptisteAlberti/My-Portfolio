523732becda3bd77004cc340480695096d842365
Baptiste Alberti <alberti.baptiste@gmail.com>
2023-08-17 10:23:04 +0200

    Troisieme depot

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="about">
     <h1>A bit about me !</h1> <br/>
-    <p>My name is Baptiste Alberti and Welcome to my personnal website. I am a junior developper from France.</p>
+    <p>My name is Baptiste Alberti and Welcome to my personnal website. I am a junior developper from France. <br/>
+    I am a former literature teacher and I wanted to become a Website Developer because I love digital stuffs and social medias. <br/>
+    You can catch me on 
+    <a href="https://twitter.com/MrMijumaru" target="_blank">Twitter</a>,
+    <a href="https://www.facebook.com/LeGemeaux" target="_blank">Facebook</a>, 
+    <a href="https://www.instagram.com/benvolio_liberta/" target="_blank">Instagram</a>, 
+    <a href="https://discord.com/channels/1087359142102061096/1087365206222569522" target="_blank">Discord</a>,
+    <a href="https://www.youtube.com/channel/UCiJNHnKjw3zs4JHkkbrhEbg" target="_blank">YouTube</a>, 
+    <a href="https://github.com/BaptisteAlberti" target="_blank">GitHub</a>, 
+    and 
+    <a href="https://www.linkedin.com/in/baptiste-alberti-b7766a285/" target="_blank">LinkedIn</a>. <br/>
+    Please enjoy your visit !</p>
   </div>
 </template>
 
--- src/views/RessourcesPage.vue
+++ src/views/RessourcesPage.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="RessourcesPage">
       <h1>This page presents my first Web productions !</h1> <br/>
+    <a href="../Images/CV/CV.png" target="_blank">My Curriculum Vitae</a> <br/>
+    <a href="../Images/Cahier des charges/Cahier des charges rendu 1.pdf" target="_blank">My first production specification</a> <br/>
+    <a href="../Images/Dynamisme site web/Dynamiser un espace commentaire.png" target="_blank">My first dynamic Website</a> <br/>
     </div>
   </template>
   
